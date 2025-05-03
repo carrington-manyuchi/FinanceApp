87a6e349ecc488c837844f8f8a6e4653a52c6891
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-05-03 15:51:43 +0200

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by Manyuchi, Carrington C on 2025/05/03.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
